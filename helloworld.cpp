da3d20201b584b1ea09805ae1761ed66fd3875c3
Remi <remilanza@gmail.com>
2023-03-03 13:42:21 +0100

    build scripts

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "useless.h"
+#include "GLFW/glfw3.h"
 
 int main()
 {
@@ -7,6 +8,9 @@ int main()
     
     int i = useless(1);
     std::cout << "useless(1) = " << i << std::endl;
+    
+    GLFWwindow* w = nullptr;
+
 
     return 0;
 }
---
+++ scripts/run.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+build/hello_world
